16af4910de554874e4972152fe4d920b03a2c3b0
Andrija <andrijagajicbusiness@gmail.com>
2021-03-03 13:55:06 +0100

    Done

--- index.py
+++ index.py
@@ -1,46 +1,136 @@
 from selenium import webdriver
-import pyperclip
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+i = 0
+while i < 10:
+	i += 1
+	PATH = "C:\Program Files (x86)\chromedriver.exe"
+	driver = webdriver.Chrome(PATH)
 
-# page to get random email address
-driver.get("https://temp-mail.org/sr/")
+	# page to get random email address
+	driver.get("https://mail.tm/en/")
 
-# field where email is stored
-random_mail = driver.find_element_by_id("mail")
-# while email is in process of making these are the values of input:
-variants = ["Учитавање", "Учитавање.", "Учитавање..", "Учитавање..."]
+	# field where email is stored
+	random_mail = driver.find_element_by_id("address")
 
-# check against all variants
-while(random_mail.get_property("value") in variants):
-	# wait .5s because it takes time
-	time.sleep(0.5)
-	random_mail = driver.find_element_by_id("mail")
+	# check against all variants
+	while(random_mail.get_property("value") == "..."):
+		# wait .5s because it takes time
+		time.sleep(0.25)
+		random_mail = driver.find_element_by_id("address")
 
-# print to check if it's successful
-print("Nova vrednost", random_mail.get_property("value"))
-access_info = {
-  "mail": random_mail.get_property("value")
-}
+	# print to check if it's successful
+	print("Nova vrednost", random_mail.get_property("value"))
+	access_info = {
+	  "mail": random_mail.get_property("value")
+	}
 
-# copy the text to clipboard
-# pyperclip.copy(random_mail.get_property("value"))
-# check if it copied
-# copied_text = pyperclip.paste()
-# print(copied_text)
+	# enter the second page because that's were the work is
+	driver.execute_script('''window.open("https://www.evropskidnevnik.rs/multimedija-kategorija/page/2/","_blank").focus();''')
+	temp_mail_page = driver.window_handles[0]
+	voting_page = driver.window_handles[2]
+	driver.switch_to.window(voting_page)
 
-# enter the second page because that's were the work is
-driver.execute_script('''window.open("https://www.evropskidnevnik.rs/multimedija-kategorija/page/2/","_blank").focus();''')
-print(len(driver.window_handles))
-driver.switch_to_window(driver.window_handles[2])
+	print("naslov:", driver.title)
+	works = driver.find_elements_by_class_name("student-single-item-data")
+	# we need last(6th) work 
+	goal_work = works[5]
+	like_btn = goal_work.find_element_by_class_name("vote-btn")
+	like_btn.click()
 
-print("naslov:", driver.title)
-works = driver.find_elements_by_class_name("student-single-item-data")
-# we need last(6th) work 
-goal_work = works[5]
-project_name = goal_work.find_element_by_class_name("total-votes").text
-print("Broj lajkova:", project_name)
+	form = driver.find_element_by_class_name("confirm-registration-data")
+	link = form.find_element_by_tag_name("a")
+	link.click()
 
-# driver.quit()
+	register_link = driver.find_element_by_link_text("Register")
+	register_link.click()
+
+	username_input = driver.find_element_by_id("user_login")
+	username_input.clear()
+	username_input.send_keys(access_info["mail"])
+
+	email_input = driver.find_element_by_id("user_email")
+	email_input.clear()
+	email_input.send_keys(access_info["mail"])
+
+	submit_form_button = driver.find_element_by_id("wp-submit")
+	submit_form_button.click()
+
+	driver.switch_to.window(temp_mail_page)
+
+	link_for_like = WebDriverWait(driver, 20).until(
+	        EC.presence_of_element_located((By.CSS_SELECTOR, "li>a"))
+	    )
+	link_for_like.click()
+
+	go_back_voting_page = WebDriverWait(driver, 20).until(
+	        EC.presence_of_element_located((By.CSS_SELECTOR, "span>a"))
+	    )
+	go_back_voting_page.click()
+
+	# open document
+	document = WebDriverWait(driver, 120).until(
+	        EC.presence_of_element_located((By.CLASS_NAME, "whitespace-pre-line"))
+	    )
+
+	# write document to local file
+	with open("file.txt", "w", encoding="utf-8") as file1:
+		file1.write(document.text)
+		file1.close()
+
+	with open("file.txt", "r", encoding="utf-8") as f:
+	    lines = f.read().splitlines()
+	    last_line = lines[-1]
+	    print(last_line)
+	    driver.execute_script('window.open("{last_line}","_blank").focus();'.format(last_line=last_line))
+
+	driver.switch_to.window(driver.window_handles[-1])
+	print(driver.title)
+
+	driver.execute_script("document.getElementById('pass1').dataset.pw = '';")
+	driver.find_element_by_id("pass1").send_keys(access_info["mail"])
+
+	reset_button = driver.find_element_by_id("wp-submit")
+	reset_button.click()
+
+	log_in_link = driver.find_element_by_css_selector("p>a")
+	log_in_link.click()
+
+
+	new_user_login = driver.find_element_by_id("user_login")
+	new_user_login.clear()
+	new_user_login.send_keys(access_info["mail"])
+
+	new_user_password = driver.find_element_by_id("user_pass")
+	new_user_password.clear()
+	new_user_password.send_keys(access_info["mail"])
+
+	# might not work when internet connection is slow
+	form_final = driver.find_element_by_id("loginform")
+	form_final.submit()
+
+	driver.execute_script('''window.open("https://www.evropskidnevnik.rs/multimedija-kategorija/page/2/","_blank").focus();''')
+	driver.switch_to.window(driver.window_handles[-1])
+
+	print(driver.title)
+
+	works = driver.find_elements_by_class_name("student-single-item-data")
+	# we need last(6th) work 
+	goal_work = works[5]
+	like_btn = goal_work.find_element_by_class_name("vote-btn")
+	like_btn.click()
+
+	# wait for like notification
+	time.sleep(3)
+	driver.delete_all_cookies() 
+	driver.execute_script('window.localStorage.clear();')
+	driver.execute_script('window.sessionStorage.clear();')
+
+	driver.quit()
+
+# staviti input - koliko lajkova zelis
+# staviti for petlju 
+# staviti try catch u slucaju greske da uradi continue
